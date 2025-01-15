12f3f828803b17b6ce8b5d3bd4224849bd85fa6e
Sushan Khatri <109749923+KhatriSuson@users.noreply.github.com>
2025-01-15 22:35:47 +0545

    inital commit

--- forensicApp/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-class ForensicappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'forensicApp'
--- forensicApp/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class SubscriptionForm(forms.Form):
-    email = forms.EmailField(label="Email", required=True)
